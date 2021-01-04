1f253b9fb35f180c7a44435f517d4d1a70a9bf2e
Shawn Murphy <smurp@smurp.com>
2021-01-05 00:42:40 +0100

    comment it all out

--- bin/grab_images.sh
+++ bin/grab_images.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-cd public/images/cards
+#cd public/images/cards
 #wget https://web.archive.org/web/20010808164154im_/http://wallymurphy.com/images/cards/{P,T}C{1,2,3,4,5,6}.jpg
-wget https://web.archive.org/web/20010808164154im_/http://wallymurphy.com/images/cards/{P,T}C{1,2,3,4,5,6}-sml.jpg
+#wget https://web.archive.org/web/20010808164154im_/http://wallymurphy.com/images/cards/{P,T}C{1,2,3,4,5,6}-sml.jpg
 
 #cd public/images/sketches/jpg
 #wget https://web.archive.org/web/20010808164154im_/http://wallymurphy.com/images/sketches/jpg/{a,b}{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,23}{a,b}.jpg
